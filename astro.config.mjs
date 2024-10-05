06fbfb2206c89a252cd8922586ded71fca7480a3
josuem317 <jowebdesigc@gmail.com>
2024-10-05 12:16:48 -0400

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), sanity({
+    projectId: 'hm9h78ua',
+    dataset: 'production',
+    useCdn: false, // See note on using the CDN
+    apiVersion: "2024-10-04",
+    studioBasePath: '/studio' // insert the current date to access the latest version of the API
+  }),
+  react(),]
 });
