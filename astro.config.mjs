00054a4be2b70799f611e22dabadf46a2c556f79
josuem317 <jowebdesigc@gmail.com>
2024-10-05 20:08:28 -0400

    sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,16 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    // Configura el caché aquí
+    cache: {
+      'Cache-Control': 'no-store',
+    },
+  },
   integrations: [tailwind(), sanity({
     projectId: 'hm9h78ua',
     dataset: 'production',
-    useCdn: true, // See note on using the CDN
+    useCdn: false, // See note on using the CDN
     apiVersion: "2024-10-04",
     studioBasePath: '/studio' // insert the current date to access the latest version of the API
   }),
--- src/sanity/lib/load-query.ts
+++ src/sanity/lib/load-query.ts
@@ -13,7 +13,6 @@ export async function loadQuery<QueryResponse>({
     params ?? {},
     { filterResponse: false }
   );
-
   return {
     data: result,
   };
